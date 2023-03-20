0e4b9aff71fd61bf43c79d51ad5c2ef4ede00e13
Everton Fonseca <everton270694@gmail.com>
2023-03-20 10:54:22 -0300

    update requirements.txt and add reqparse to receive aws region as json data paramether on body request

--- app/resources/ec2_list_resource.py
+++ app/resources/ec2_list_resource.py
@@ -1,12 +1,17 @@
 from flask_restful import Resource
+from flask_restful import reqparse
 import boto3
 
-
+profile = "tf"
 
 class Ec2ListResource(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('region', type=str, required=True, help='aws region is a required paramether, please send desired region as header')
+
     def get(self):
-        region = 'us-east-1'
-        session = boto3.Session(profile_name='hashicorp', region_name=region)
+        data_paramether = Ec2ListResource.parser.parse_args()
+        region = data_paramether['region']
+        session = boto3.Session(profile_name=profile, region_name=region)
         ec2 = session.client('ec2')
         check_ec2 = ec2.describe_instances()['Reservations']
         ec2_list = []
