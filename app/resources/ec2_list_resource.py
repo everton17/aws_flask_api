0f56a11f3454f6ce4522aec1f1d5ecc2116d4e6b
Everton Fonseca <everton270694@gmail.com>
2023-03-20 13:07:30 -0300

    add botocore exeptions on ec2_list endpoint for fix error when user send invalid aws region on request

--- app/resources/ec2_list_resource.py
+++ app/resources/ec2_list_resource.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource
 from flask_restful import reqparse
+import botocore
 import boto3
 
 profile = "tf"
@@ -13,8 +14,12 @@ class Ec2ListResource(Resource):
         region = data_paramether['region']
         session = boto3.Session(profile_name=profile, region_name=region)
         ec2 = session.client('ec2')
-        check_ec2 = ec2.describe_instances()['Reservations']
         ec2_list = []
+        try:
+            check_ec2 = ec2.describe_instances()['Reservations']
+        except botocore.exceptions.EndpointConnectionError:
+            message = {"error": "Endpoint Connection Error, check if you send a valid aws region as json format on your body request"}
+            return message
         if not len(check_ec2) > 0:
             message = {"message": f"Dont exists EC2 Intances on {region} region"}
             return message
